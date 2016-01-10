3e53fc950eed7a2a99b334a994516e6d8555d632
David Best <david.best@gsa.gov>
2016-01-09 22:06:10 -0500

    smaller timestamps

--- test-bandit.sh
+++ test-bandit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install bandit
-bandit code/ -r -f json -o results/bandit.$(date -u +"%Y%m%dT%H%M%SZ").json
+bandit code/ -r -f json -o results/bandit.$(date -u +"%Y%m%d.%H%M%S").json
 exit 0
--- test-owasp.sh
+++ test-owasp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade git+https://github.com/Grunny/zap-cli.git
 
-zap-cli quick-scan --spider -r -sc -o '-config api.disablekey=true' $LIVE_TARGET > results/zap.$(date -u +"%Y%m%dT%H%M%SZ").txt
+zap-cli quick-scan --spider -r -sc -o '-config api.disablekey=true' $LIVE_TARGET > results/zap.$(date -u +"%Y%m%d.%H%M%S").txt
 exit 0
