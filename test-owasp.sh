e0f6cf7cb533b57d9f1ebba91adb13493a900d5d
David Best <david.best@gsa.gov>
2016-01-28 10:56:30 -0500

    Getting HTML as well

--- test-owasp.sh
+++ test-owasp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#pip install --upgrade git+https://github.com/Grunny/zap-cli.git
 
 zap-cli start --start-options '-config api.disablekey=true'
 zap-cli open-url $LIVE_TARGET
 zap-cli active-scan -s all -r $LIVE_TARGET
 zap-cli alerts -l Low -f json > results/zap.json
+zap-cli alerts -l Low -f html > results/zap.html
 zap-cli shutdown
 
 exit 0
