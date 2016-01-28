6fc2936d4b95824039633492ba04d341b068b100
David Best <david.best@gsa.gov>
2016-01-28 14:07:45 -0500

    Helps to spider first.

--- test-owasp.sh
+++ test-owasp.sh
@@ -2,9 +2,9 @@
 
 zap-cli start --start-options '-config api.disablekey=true'
 zap-cli open-url $LIVE_TARGET
+zap-cli spider $LIVE_TARGET
 zap-cli active-scan -s all -r $LIVE_TARGET
-zap-cli alerts -l Low -f json > results/zap.json
-zap-cli alerts -l Low -f html > results/zap.html
+zap-cli alerts -l Informational -f json > results/zap.json
 zap-cli shutdown
 
 exit 0
