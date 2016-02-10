25a2b086365337d730bcb56d800d43145ff431b9
David Best <david.best@gsa.gov>
2016-02-10 08:35:12 -0500

    Leaving ZAP up between runs allowed alerts to accrue between targets.

--- test-owasp.sh
+++ test-owasp.sh
@@ -6,22 +6,23 @@ pip install tinys3
 COUNTER=0
 COUNT=$(cat scripts/targets.json | jq '.targets[] .url' | wc -l)
 
-zap-cli start --start-options '-config api.disablekey=true'
 
 while [ $COUNTER -lt $COUNT ]; do
   NAME=$(cat scripts/targets.json | jq ".targets[${COUNTER}] .name" -r)
   TARGET=$(cat scripts/targets.json | jq ".targets[${COUNTER}] .url" -r)
 
   echo Scanning $NAME: $TARGET
+  # Leaving ZAP up between runs allows alerts to accrue
+  zap-cli start --start-options '-config api.disablekey=true'
   zap-cli open-url $TARGET
   zap-cli spider $TARGET
   zap-cli active-scan -s all -r $TARGET
   zap-cli alerts -l Informational -f json > results/${NAME}.json
+  zap-cli shutdown
 
   let COUNTER+=1
 done
 
-zap-cli shutdown
 
 echo Uploading files...
 python scripts/s3upload.py
