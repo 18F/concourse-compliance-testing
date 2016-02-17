05157d7b11e00332d773cdbaa65d4ba070a17791
Aidan Feldman <aidan.feldman@gsa.gov>
2016-02-16 23:45:27 -0500

    install all eslint dependencies with one command

--- test-eslint.sh
+++ test-eslint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-npm install -g eslint
-npm install -g eslint-config-angular
-npm install -g eslint-plugin-angular
-npm install -g eslint-config-dustinspecker
+npm install -g eslint eslint-config-angular eslint-plugin-angular eslint-config-dustinspecker
 eslint . -f json -o results/eslint.json --ignore-path .gitignore
