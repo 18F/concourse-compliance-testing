5996aa74d62df20b990f709da51baf37363e993a
Aidan Feldman <aidan.feldman@gsa.gov>
2016-03-20 03:31:38 -0400

    add more tests to TeamDataFilterer

--- tasks/filter-team-data/test/team_data_filterer.rb
+++ tasks/filter-team-data/test/team_data_filterer.rb
@@ -6,23 +6,56 @@ describe TeamDataFilterer do
     it "returns the list of projects, filtered by name" do
       projects = [
         {
-          "name": "foo",
-          "something": 6
+          "name" => "foo",
+          "something" => 6
         },
         {
-          "name": "bar",
-          "something_else": 7
+          "name" => "bar",
+          "something_else" => 7
         }
       ]
       targets = [
-        { "name": "bar" }
+        { "name" => "bar" }
       ]
 
       results = TeamDataFilterer.filtered_projects(projects, targets)
       expect(results).must_equal [
         {
-          "name": "bar",
-          "something_else": 7
+          "name" => "bar",
+          "something_else" => 7
+        }
+      ]
+    end
+
+    it "handles a target not present in the projects" do
+      targets = [
+        { "name" => "foo" }
+      ]
+
+      results = TeamDataFilterer.filtered_projects([], targets)
+      expect(results).must_equal []
+    end
+
+    it "merges in attributes from the target" do
+      projects = [
+        {
+          "name" => "foo",
+          "something" => 6
+        },
+      ]
+      targets = [
+        {
+          "name" => "foo",
+          "something_else" => 7
+        }
+      ]
+
+      results = TeamDataFilterer.filtered_projects(projects, targets)
+      expect(results).must_equal [
+        {
+          "name" => "foo",
+          "something" => 6,
+          "something_else" => 7
         }
       ]
     end
