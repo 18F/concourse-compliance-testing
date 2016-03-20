26dea2937740a23a010d4a69fd52b11fd6d4be50
Aidan Feldman <aidan.feldman@gsa.gov>
2016-03-20 03:09:35 -0400

    create a TeamDataFilterer module

--- tasks/filter-team-data/task.rb
+++ tasks/filter-team-data/task.rb
@@ -1,5 +1,5 @@
 require 'json'
-require 'set'
+require_relative 'team_data_filterer'
 
 PROJECT_JSON = ENV['PROJECT_JSON'] || File.expand_path('../../../../projects-json/projects.json', __FILE__)
 TARGETS = File.expand_path('../../../targets.json', __FILE__)
@@ -8,24 +8,7 @@ RESULTS = ENV['RESULTS'] || File.expand_path('../../../../results/projects.json'
 projects = JSON.load(File.new(PROJECT_JSON))
 targets = JSON.load(File.new(TARGETS))
 
-projects_by_name = {}
-projects['results'].each do |project|
-  name = project['name']
-  projects_by_name[name] = project
-end
-
-filtered_projects = []
-targets.each do |target|
-  name = target['name']
-  project = projects_by_name[name]
-  if project
-    # copy in overridden attributes from the targets.json
-    filtered_project = project.merge(target)
-    filtered_projects << filtered_project
-  else
-    STDERR.puts "WARN: `#{name}` is missing from Team API data."
-  end
-end
+filtered_projects = TeamDataFilterer.filtered_projects(projects, targets)
 
 JSON.dump(filtered_projects, File.new(RESULTS, 'w'))
 
---
+++ tasks/filter-team-data/team_data_filterer.rb
@@ -0,0 +1,34 @@
+module TeamDataFilterer
+  class << self
+    def projects_by_name(projects)
+      results = {}
+      projects['results'].each do |project|
+        name = project['name']
+        results[name] = project
+      end
+      results
+    end
+
+    # copy in overridden attributes from the target
+    def transform_project(project, target)
+      project.merge(target)
+    end
+
+    def filtered_projects(projects, targets)
+      p_by_name = self.projects_by_name(projects)
+
+      results = []
+      targets.each do |target|
+        name = target['name']
+        project = p_by_name[name]
+        if project
+          results << transform_project(project, target)
+        else
+          STDERR.puts "WARN: `#{name}` is missing from Team API data."
+        end
+      end
+
+      results
+    end
+  end
+end
