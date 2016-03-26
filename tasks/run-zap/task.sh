2b1f3e414bed5c9d1e4f4e18ec66654130e18bb7
Aidan Feldman <aidan.feldman@gsa.gov>
2016-03-26 12:40:33 -0400

    simplify ZAP pipeline to only run on one project

--- tasks/run-zap/task.sh
+++ tasks/run-zap/task.sh
@@ -6,23 +6,16 @@ set -x
 apt-get install jq
 pip install --upgrade zapcli
 
-COUNTER=0
-COUNT=$(jq length < filtered-projects/projects.json)
+NAME=$(jq -r .name < project-data/project.json)
 
 zap-cli start --start-options '-config api.disablekey=true'
 
-while [ "$COUNTER" -lt "$COUNT" ]; do
-  NAME=$(jq -r ".[${COUNTER}] .name" < filtered-projects/projects.json)
-  # TODO scan all links
-  TARGET=$(jq -r ".[${COUNTER}] .links | .[0] | .url" < filtered-projects/projects.json)
+# TODO scan all links
+TARGET=$(jq -r '.links | .[0] | .url' < project-data/project.json)
 
-  echo "Scanning $NAME: $TARGET"
-  zap-cli -v quick-scan --spider --ajax-spider --scanners all "$TARGET"
-  # `zap-cli alerts` returns an error code if there are any warnings - ignore them so the script doesn't fail
-  zap-cli alerts -l Informational -f json > "results/${NAME}.json" || true
-  zap-cli session new
-
-  let COUNTER+=1
-done
+echo "Scanning $NAME: $TARGET"
+# zap-cli returns an error code if there are any warnings - ignore them so the script doesn't fail
+zap-cli -v quick-scan --spider --ajax-spider --scanners all "$TARGET" || true
+zap-cli alerts -l Informational -f json > "results/${NAME}.json" || true
 
 zap-cli shutdown
