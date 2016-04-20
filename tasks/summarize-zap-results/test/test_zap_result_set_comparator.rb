d9a0bafec8a1002f9124d3e297800d1ce235a2e5
ctro <clint@ctro.net>
2016-04-20 12:56:08 -0600

    Fix spacing

--- tasks/summarize-zap-results/test/test_zap_result_set_comparator.rb
+++ tasks/summarize-zap-results/test/test_zap_result_set_comparator.rb
@@ -20,11 +20,11 @@ class TestZAPResultSetComparator < MiniTest::Test
 
     it "should report NO CHANGE for two empty zap error sets" do
       Dir.mktmpdir do |output_dir|
-       comparator = ZAPResultSetComparator.new('fake-site-0', last_results_dir, curr_results_dir)
-       comparator.write_summary(output_dir)
-       summary_txt = File.read("#{output_dir}/summary.txt")
-       expected = "Completed scan of fake-site-0: (0/0/0/0) NO CHANGE\n<https://compliance-viewer.18f.gov/results/fake-site-0/current|View results>"
-       assert_equal expected, summary_txt
+        comparator = ZAPResultSetComparator.new('fake-site-0', last_results_dir, curr_results_dir)
+        comparator.write_summary(output_dir)
+        summary_txt = File.read("#{output_dir}/summary.txt")
+        expected = "Completed scan of fake-site-0: (0/0/0/0) NO CHANGE\n<https://compliance-viewer.18f.gov/results/fake-site-0/current|View results>"
+        assert_equal expected, summary_txt
       end
     end
   end
