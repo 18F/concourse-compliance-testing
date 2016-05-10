8e33f1ebe717096ded60afeaa082bf6c6cf6c321
Aidan Feldman <aidan.feldman@gsa.gov>
2016-05-09 20:45:46 -0400

    remove unnecessary class definitions around tests

--- tasks/summarize-zap-results/test/test_zap_project.rb
+++ tasks/summarize-zap-results/test/test_zap_project.rb
@@ -1,15 +1,13 @@
 require 'minitest/autorun'
 require_relative '../lib/zap_project'
 
-class TestZAPProject < MiniTest::Test
-  describe ZAPProject do
-    last_results_dir = "#{__dir__}/last_run"
+describe ZAPProject do
+  last_results_dir = "#{__dir__}/last_run"
 
-    describe '.project_path' do
-      it "should return the correct path for a project" do
-        project = ZAPProject.new('fake-site-3', last_results_dir)
-        assert_equal("#{__dir__}/last_run/fake-site-3.json", project.project_path)
-      end
+  describe '.project_path' do
+    it "should return the correct path for a project" do
+      project = ZAPProject.new('fake-site-3', last_results_dir)
+      assert_equal("#{__dir__}/last_run/fake-site-3.json", project.project_path)
     end
   end
 end
