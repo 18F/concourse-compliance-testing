43b998bdea03d01317dbf4df4d8abc651bad76af
Aidan Feldman <aidan.feldman@gsa.gov>
2016-06-30 15:05:50 -0400

    specify all links in the targets file explicitly

--- config/test_targets.rb
+++ config/test_targets.rb
@@ -2,11 +2,20 @@ require 'minitest/autorun'
 require_relative '../lib/team_data_filterer'
 
 describe 'targets.json' do
-  it "has all lower case names" do
+  def targets_data
     filename = File.expand_path('targets.json', __dir__)
-    data = TeamDataFilterer.read_json(filename)
-    data.each do |project|
+    TeamDataFilterer.read_json(filename)
+  end
+
+  it "has all lower case names" do
+    targets_data.each do |project|
       project['name'].wont_match(/[A-Z]/)
+    end
+  end
+
+  it "contains links" do
+    targets_data.each do |target|
+      target['links'].class.must_equal(Array)
     end
   end
 end
